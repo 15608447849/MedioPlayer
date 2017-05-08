2d671a2207b679d6bd4e0b02c0e704ed83ef6237
YoungZhi2016 <github13142054543>
2017-05-08 16:21:31 +0800

    电子报图片返回，上下一页

--- app/src/main/java/com/wos/play/rootdir/model_application/viewlayer/EpaperActivity.java
+++ app/src/main/java/com/wos/play/rootdir/model_application/viewlayer/EpaperActivity.java
@@ -1,31 +1,34 @@
 package com.wos.play.rootdir.model_application.viewlayer;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.ImageButton;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.wos.play.rootdir.R;
 import com.wos.play.rootdir.model_application.baselayer.BaseActivity;
-import com.wos.play.rootdir.model_application.ui.ComponentLibrary.epapers.EActivityGrallyAdpter;
+import com.wos.play.rootdir.model_application.ui.Uitools.UiTools;
 import com.wos.play.rootdir.model_universal.tool.Logs;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 import cn.trinea.android.common.util.FileUtils;
 
-public class EpaperActivity extends BaseActivity {
+public class EpaperActivity extends BaseActivity implements View.OnClickListener {
 
     public static final String TAG = "EpaperActivity";
     public static final String PATHKEY = "paperFilepath";
-    private GridView list;
-    private EActivityGrallyAdpter adpter;
+    private ArrayList<String> sourceList;
     private SubsamplingScaleImageView imageView;
+
+    private ImageButton epaper_imageButton_back;//返回
+    private ImageButton epaper_imageButton_previous;//上一页
+    private ImageButton epaper_imageButton_next;//下一页
+    private static int index = 0;//第几页
 
     //适配器
     @Override
@@ -33,8 +36,30 @@ public class EpaperActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_epaper);
         setIsOnBack(true);
+
         initIntent();
         initView();
+        initData(0);
+    }
+
+    /**
+     * 初始化数据
+     *
+     * @param type
+     */
+    private void initData(int type) {
+        close();
+        switch (type) {
+            case 1://上一页就减
+                index--;
+                break;
+            case 2:
+                index++;
+                break;
+        }
+        if (index >= sourceList.size()) index = 0;
+        else if (index < 0) index = sourceList.size() - 1;
+        imageView.setImage(ImageSource.uri(sourceList.get(index)));
     }
 
     //初始化路径
@@ -46,58 +71,37 @@ public class EpaperActivity extends BaseActivity {
         }
         Logs.i(TAG, "打开电子报 - [ " + path + " ]");
         File[] files = new File(path).listFiles();
-        List<File> arrList = new ArrayList<>();
-        for (int i = 0; i < files.length; i++) {
-            File sFile = files[i];
+
+        sourceList = new ArrayList<>();//电子报图片路径list
+        for (File sFile : files) {
             if (sFile.isDirectory() && sFile.list().length > 0) {
-                arrList.add(sFile);
+                sourceList.add(getSourceImagePath(sFile));
             }
         }
-        Logs.i(TAG, "列表 - [ " + arrList + " ]");
-        if (arrList.size() > 0) {
-            adpter = new EActivityGrallyAdpter(this, arrList);
-        } else {
-            stopActivityOnArr(this);
-        }
     }
-
 
     // 返回键
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return super.onKeyDown(keyCode, event);
+        return close();
     }
 
     //初始化视图
     private void initView() {
-        list = (GridView) findViewById(R.id.grid);
-        list.setAdapter(adpter);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String path = adpter.getSourceImagePath(adpter.getSource(position));
-                Logs.i(TAG, "选择: " + path);
-                if (imageView != null && imageView.getVisibility() == View.GONE) {
-                    list.setVisibility(View.GONE);
-                    imageView.setVisibility(View.VISIBLE);
-                    imageView.setImage(ImageSource.uri(path));
-                }
-            }
-        });
-
         imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
-        imageView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                if (imageView != null && imageView.getVisibility() == View.VISIBLE) {
-                    imageView.recycle();
-                    imageView.setVisibility(View.GONE);
-                    list.setVisibility(View.VISIBLE);
-                }
-                return true;
-            }
-        });
+        epaper_imageButton_back = (ImageButton) findViewById(R.id.epaper_imageButton_back);
+        epaper_imageButton_previous = (ImageButton) findViewById(R.id.epaper_imageButton_previous);
+        epaper_imageButton_next = (ImageButton) findViewById(R.id.epaper_imageButton_next);
 
+        //设置透明度
+        epaper_imageButton_back.setBackgroundColor(Color.TRANSPARENT);
+        epaper_imageButton_previous.setBackgroundColor(Color.TRANSPARENT);
+        epaper_imageButton_next.setBackgroundColor(Color.TRANSPARENT);
+
+        //设置监听器
+        epaper_imageButton_next.setOnClickListener(this);
+        epaper_imageButton_back.setOnClickListener(this);
+        epaper_imageButton_previous.setOnClickListener(this);
     }
 
     @Override
@@ -106,7 +110,56 @@ public class EpaperActivity extends BaseActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        close();
+        stopActivityOnArr(this);
+    }
+
+    private boolean close() {
+        if (imageView != null) {
+            imageView.recycle();
+            System.gc();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.epaper_imageButton_back://返回
+                close();
+                stopActivityOnArr(this);
+                break;
+            case R.id.epaper_imageButton_previous://上一页
+                initData(1);
+                break;
+            case R.id.epaper_imageButton_next://下一页
+                initData(2);
+                break;
+        }
+    }
+
+    //获取电子报图片路径
+    public String getSourceImagePath(File source) {
+        if (source != null) {
+            //循环遍历 - 找出 文件名 thumb_开头的文件
+            String[] list = source.list();
+            if (list != null && list.length > 0) {
+                for (String aList : list) {
+                    if (!aList.contains("thumb") && aList.contains(".png"))
+                        return source + "/" + aList;
+                }
+            }
+        }
+        return UiTools.getDefImagePath();
     }
 }
